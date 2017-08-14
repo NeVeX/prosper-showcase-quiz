8bce04966053a7147276a446c0e47615ec092172
Eric <eheitmuller@gmail.com>
2017-08-14 14:55:57 -0700

    adding more tests

--- test/services/question-validator.tests.js
+++ test/services/question-validator.tests.js
@@ -26,4 +26,111 @@ describe("question validator service", function(){
         });
     });
 
+    it("should reject a missing answer 1", function(done){
+        var questionJson = {
+            "question": "What does it say on the wall when coming out of our 3rd floor elevator?",
+            "answerTwo": "Happy pride week",
+            "answerThree": "It's good to see you",
+            "answerFour": "Have a great day",
+            "correctAnswer": 3,
+            "timeAllowedSeconds": 32
+        };
+
+        validator.validateQuestion(questionJson)
+        .then(function(result){
+            assert.fail();
+            done();
+        })
+        .catch(function(err){
+           assert.equal(err.message, "Answer one is not valid");
+           done();
+        });
+    });
+
+    it("should reject a missing answer 2", function(done){
+        var questionJson = {
+            "question": "What does it say on the wall when coming out of our 3rd floor elevator?",
+            "answerOne": "Welcome to Prosper",
+            "answerThree": "It's good to see you",
+            "answerFour": "Have a great day",
+            "correctAnswer": 3,
+            "timeAllowedSeconds": 32
+        };
+
+        validator.validateQuestion(questionJson)
+        .then(function(result){
+            assert.fail();
+            done();
+        })
+        .catch(function(err){
+           assert.equal(err.message, "Answer two is not valid");
+           done();
+        });
+    });
+
+    it("should reject a missing answer 3", function(done){
+        var questionJson = {
+            "question": "What does it say on the wall when coming out of our 3rd floor elevator?",
+            "answerOne": "Welcome to Prosper",
+            "answerTwo": "It's good to see you",
+            "answerFour": "Have a great day",
+            "correctAnswer": 3,
+            "timeAllowedSeconds": 32
+        };
+
+        validator.validateQuestion(questionJson)
+        .then(function(result){
+            assert.fail();
+            done();
+        })
+        .catch(function(err){
+           assert.equal(err.message, "Cannot have answer four when there is no answer three");
+           done();
+        });
+    });
+
+    it("should reject a question with a missing answer", function(done){
+        var questionJson = {
+            "question": "What does it say on the wall when coming out of our 3rd floor elevator?",
+            "answerOne": "Welcome to Prosper",
+            "answerTwo": "Happy pride week",
+            "answerThree": "It's good to see you",
+            "answerFour": "Have a great day",
+            "timeAllowedSeconds": 32
+        };
+
+        validator.validateQuestion(questionJson)
+        .then(function(result){
+            assert.fail();
+            done();
+        })
+        .catch(function(err){
+           assert.equal(err.message, "There is no valid correct answer");
+           done();
+        });
+
+    });
+
+    it("should reject a question with an invalid timeAllowed", function(done){
+        var questionJson = {
+            "question": "What does it say on the wall when coming out of our 3rd floor elevator?",
+            "answerOne": "Welcome to Prosper",
+            "answerTwo": "Happy pride week",
+            "answerThree": "It's good to see you",
+            "answerFour": "Have a great day",
+            "correctAnswer": 3
+        };
+
+        validator.validateQuestion(questionJson)
+        .then(function(result){
+            assert.fail();
+            done();
+        })
+        .catch(function(err){
+           assert.equal(err.message, "The time allowed in seconds is not valid");
+           done();
+        });
+
+    });
+
 });
