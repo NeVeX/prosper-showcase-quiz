2b329deb8300af1c79d6de6d0eb1e330e993ac14
Eric <eheitmuller@gmail.com>
2017-08-14 17:58:36 -0700

    finished moving all routes to logical files

--- routes/static-routes.js
+++ routes/static-routes.js
@@ -1,5 +1,9 @@
 
 module.exports = function(app){
+
+    var landingPageHtml = fs.readFileSync('static/landing-page.html');
+    var questionsHtml = fs.readFileSync('static/question.html');
+
     app.get('/prosperquiz', function(request, response) {
         response.setHeader('Content-Type', 'text/html');
         return response.status(200).end(landingPageHtml);
